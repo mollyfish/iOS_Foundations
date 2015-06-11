452fa1dea042faeb7013d36bd70ccada53eafc8d
Molly Kent <kent.molly@gmail.com>
2015-06-10 19:46:00 -0700

    playing with segues

---
+++ TableViewPlay/TableViewPlay/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  TableViewPlay
+//
+//  Created by Molly Kent on 6/10/15.
+//  Copyright (c) 2015 Molly Kent. All rights reserved.
+//
+
+import Foundation
---
+++ TableViewPlay/TableViewPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPlay
+//
+//  Created by Molly Kent on 6/10/15.
+//  Copyright (c) 2015 Molly Kent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
